5b10f546771dd52d112029d8acf7e785eec6c873
Mathieu Legault <mlegault@inocybe.com>
2014-11-20 16:14:17 -0500

    Added a modal window for optional features
    You can now select which optional features you want
    to include.
    
    Change-Id: I0a88c0588e94b5da72a71bae77abab7930a04d49
    Signed-off-by: Mathieu Legault <mlegault@inocye.com>

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,53 +1,15 @@
 $(document).ready(function(){
 	/* The following code is executed once the DOM is loaded */
 
-	$('.featuresSelected').hide();
-
-	$('.componentFlip').bind("click",function(){
-
-		// $(this) point to the clicked .sponsorFlip element (caching it in elem for speed):
-
-		var elem = $(this);
-
-		// data('flipped') is a flag we set when we flip the element:
-
-		if(elem.data('flipped'))
-		{
-			// If the element has already been flipped, use the revertFlip method
-			// defined by the plug-in to revert to the default state automatically:
-
-			elem.revertFlip();
-			elem.parent().parent().children('.featuresSelected').hide();
-			// Unsetting the flag:
-			elem.data('flipped',false)
-		}
-		else
-		{
-			// Using the flip method defined by the plugin:
-
-			elem.flip({
-				direction:'lr',
-				speed: 350,
-				onBefore: function(){
-					// Insert the contents of the .sponsorData div (hidden from view with display:none)
-					// into the clicked .sponsorFlip div before the flipping animation starts:
-					elem.parent().parent().children('.featuresSelected').show();
-					elem.html(elem.siblings('.componentSelected').html());
-				}
-			});
-
-			// Setting the flag:
-			elem.data('flipped',true);
-		}
-	});
-
 	$('#bigbutton').click(function() {
 		var flipped = 0;
 		var features = [];
 		$('.componentFlip').each(function() {
 			var elem = $(this);
 			if(elem.data('flipped')) {
-				elem.parent().parent().children('.featuresSelected').children("input:checkbox:checked").each(function() {
+				var id = elem.attr('data-modal');
+				var children = $('#' + id).find('input');
+				$('#' + id).find("input:checkbox:checked").each(function() {
 					var feature = $(this).val();
 					features.push(feature)
 				});
@@ -62,5 +24,4 @@ $(document).ready(function(){
 			data: result
 		});
 	});
-
 });
